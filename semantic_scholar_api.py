655c9d06c588e8e4d895fd6a4c02967591a95941
seraphinatarrant <serif@uw.edu>
2022-06-17 16:08:27 +0100

    filter by influential or sections, also examples results

--- semantic_scholar_api.py
+++ semantic_scholar_api.py
@@ -1,6 +1,17 @@
+"""
+Takes in yaml config specifying seed paper IDs (arxiv, DOI, semantic scholar IDs)
+to look up references and citations for. Generates a citation csv and reference csv per paper,
+and also a union csv that includes which seed papers voted for which references/citations.
+
+Optionally can specify to include only influential papers or only ones referenced or cited
+in certain sections (currently doesn't differentiate between references and citations though could
+if useful.
+"""
+
 import argparse
 import os
 import time
+from collections import defaultdict
 from typing import List
 from tqdm import tqdm
 
@@ -9,6 +20,7 @@ import s2
 import csv
 
 HEADERS = ["Title", "Section", "Influential", "Year", "Authors", "Abstract", "URL", "DOI", "Arxiv"]
+SUMMARY_HEADERS = ["Cites", "ReferencedBy"]
 
 
 def make_row(p) -> List:
@@ -42,11 +54,31 @@ def make_paper_reference(paper) -> str:
     return f"{first_author_name}{paper.year}"
 
 
+def check_include(p, config) -> bool:
+    if config["only_influential"]:
+        if not p.isInfluential:
+            return False
+    if config["only_include_sections"]:
+        paper_sections = set(p.intent)
+        if not paper_sections:  # this section is missing, so include it
+            return True
+
+        include_sections = set(config["only_include_sections"])
+        if not include_sections & paper_sections:
+            return False
+    return True
+
+
+
 if __name__ == "__main__":
     args = setup_argparse()
 
     print(f"Reading config {args.config_file}...")
     config = read_yaml_config(args.config_file)
+
+    #TODO finish union csv
+    #seed2paper_ref = defaultdict(lambda: defaultdict(list))  # for generating union, which adds columns
+    #all_papers = set() # also for union csv at end
 
     for pid in config["seed_paper_ids"]:
         paper = s2.api.get_paper(paperId=pid)
@@ -59,14 +91,26 @@ if __name__ == "__main__":
         curr_count = 1
         print(f"{len(paper.citations)} Citations")
         for c in tqdm(paper.citations):
+            if not check_include(c, config):
+                continue
+            #seed2paper_ref[c.paperID]["citation"].append(root_paper)
+            #all_papers.add(c)
             citation_rows.append(make_row(c))
             curr_count = check_rate_limit(curr_count, max_call)
         print(f"{len(paper.references)} References")
         for r in tqdm(paper.references):
+            if not check_include(r, config):
+                continue
+            #seed2paper_ref[c.paperID]["reference"].append(root_paper)
+            #all_papers.add(c)
             reference_rows.append(make_row(r))
             curr_count = check_rate_limit(curr_count, max_call)
 
         # write a csv of them
+        output_dir = config["output_dir"]
+        if not os.path.exists(output_dir):
+            print(f"Creating {output_dir} output path")
+            os.makedirs(output_dir)
         file2papers = {
             os.path.join(config["output_dir"], f"{root_paper}_citations.csv"): citation_rows,
             os.path.join(config["output_dir"], f"{root_paper}_references.csv"): reference_rows
@@ -77,4 +121,10 @@ if __name__ == "__main__":
                 csvwriter.writerow(HEADERS)
                 for row in papers:
                     csvwriter.writerow(row)
+
+    # make union csv TODO
+
+
+
+
 
